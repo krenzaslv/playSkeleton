7984a5ca849c6c6567e1a74516b2a27c6f693c92
Daniel Widmer <krenzaslv@gmail.com>
2014-12-19 21:55:22 +0100

    cleanup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,11 +5,6 @@ import services.UUIDGenerator
 import org.slf4j.{LoggerFactory, Logger}
 import play.api.mvc._
 
-/**
- * Instead of declaring an object of Application as per the template project, we must declare a class given that
- * the application context is going to be responsible for creating it and wiring it up with the UUID generator service.
- * @param uuidGenerator the UUID generator service we wish to receive.
- */
 @Singleton
 class Application @Inject() (uuidGenerator: UUIDGenerator) extends Controller {
 
--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -14,45 +14,21 @@ import services.UUIDGenerator
 
 import scala.concurrent.Future
 
-/**
- * The Users controllers encapsulates the Rest endpoints and the interaction with the MongoDB, via ReactiveMongo
- * play plugin. This provides a non-blocking driver for mongoDB as well as some useful additions for handling JSon.
- * @see https://github.com/ReactiveMongo/Play-ReactiveMongo
- */
 @Singleton
 class Users extends Controller with MongoController {
 
 
   private final val logger = Logger
 
-  /*
-   * Get a JSONCollection (a Collection implementation that is designed to work
-   * with JsObject, Reads and Writes.)
-   * Note that the `collection` is not a `val`, but a `def`. We do _not_ store
-   * the collection reference to avoid potential problems in development with
-   * Play hot-reloading.
-   */
   def collection: JSONCollection = db.collection[JSONCollection]("users")
-
-  // ------------------------------------------ //
-  // Using case classes + Json Writes and Reads //
-  // ------------------------------------------ //
 
   import models._
   import models.JsonFormats._
 
   def createUser = Action.async(parse.json) {
     request =>
-    /*
-     * request.body is a JsValue.
-     * There is an implicit Writes that turns this JsValue as a JsObject,
-     * so you can call insert() with this JsValue.
-     * (insert() takes a JsObject as parameter, or anything that can be
-     * turned into a JsObject using a Writes.)
-     */
       request.body.validate[User].map {
         user =>
-        // `user` is an instance of the case class `models.User`
           collection.insert(user).map {
             lastError =>
               logger.debug(s"Successfully inserted with LastError: $lastError")
@@ -62,23 +38,16 @@ class Users extends Controller with MongoController {
   }
 
   def findUsers = Action.async {
-    // let's do our query
     val cursor: Cursor[User] = collection.
-      // find all
       find(Json.obj("active" -> true)).
-      // sort them by creation date
       sort(Json.obj("created" -> -1)).
-      // perform the query and get a cursor of JsObject
       cursor[User]
 
-    // gather all the JsObjects in a list
     val futureUsersList: Future[List[User]] = cursor.collect[List]()
 
-    // transform the list into a JsArray
     val futurePersonsJsonArray: Future[JsArray] = futureUsersList.map { users =>
       Json.arr(users)
     }
-    // everything's ok! Let's reply with the array
     futurePersonsJsonArray.map {
       users =>
         Ok(users(0))
